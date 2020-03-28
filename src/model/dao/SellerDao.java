e597cce0a8e6f1a180e2e1cf83166bc6f2036da4
Jean Pierre <tavares1988@live.com>
2020-03-28 18:08:25 -0300

    Correção de erro no nome das classes (DepartmentDao e SellerDao) e criação das classes SellerDaoJDBC, DepartmentDaoJDBC e DaoFactory

--- src/model/dao/SellerDao.java
+++ src/model/dao/SellerDao.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import model.entities.Seller;
 
-public interface ClientDao {
+public interface SellerDao {
 
 	void insert(Seller obj);
 	void update(Seller obj);
-	void deletById(Seller obj);
-	Seller findById(Seller obj);
+	void deletById(Integer id);
+	Seller findById(Integer id);
 	List<Seller> findAll();
 }
