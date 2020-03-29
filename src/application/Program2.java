fc80eadc170ce1d6cfdb4f39e485c4d6e0175907
Jean Pierre <tavares1988@live.com>
2020-03-29 16:07:46 -0300

    Implementação do método findAll na classe DepartmentDaoJDBC

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -9,13 +11,19 @@ public class Program2 {
 	public static void main(String[] args) {
 		
 		System.out.println("==== TESTE 1: department insert ====");
-		Department newDepartment = new Department(null, "Telecomunicação");
+		Department newDepartment = new Department(null, "Administração");
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New Id = " + newDepartment.getId());
 		
-		System.out.println("==== TESTE 2: department findById ====");
-		Department department = departmentDao.findById(3);
+		System.out.println("\n==== TESTE 2: department findById ====");
+		Department department = departmentDao.findById(5);
 		System.out.println(department);
+		
+		System.out.println("\n==== TESTE 3: department findAll ====");
+		List<Department> list = departmentDao.findAll();
+		for(Department dep : list) {
+			System.out.println(dep);
+		}
 	}
 }
