dc327b684993ead83aef9ceae9e3720ff28809ec
Jean Pierre <tavares1988@live.com>
2020-03-29 16:31:06 -0300

    Implementação do método delete na classe DepartmentDaoJDBC. OBS.: o método update foi implementado e comitado junto com o método findById

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -9,9 +10,10 @@ import model.entities.Department;
 public class Program2 {
 
 	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
 		
 		System.out.println("==== TESTE 1: department insert ====");
-		Department newDepartment = new Department(null, "Administração");
+		Department newDepartment = new Department(null, "Gestão");
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! New Id = " + newDepartment.getId());
@@ -25,5 +27,18 @@ public class Program2 {
 		for(Department dep : list) {
 			System.out.println(dep);
 		}
+		
+		System.out.println("\n==== TESTE 4: department update/delete ====");
+		department = departmentDao.findById(5);
+		department.setName("Marketing & Design");
+		departmentDao.update(department);
+		System.out.println("Update completed!");
+		
+		System.out.print("Informe Id do departamento a ser deletado: ");
+		int id = teclado.nextInt();
+		departmentDao.deletById(id);
+		System.out.println("Delete completed!");
+		
+		teclado.close();
 	}
 }
